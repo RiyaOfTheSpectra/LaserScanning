899ead6882552974f70d1bc700d435595b6eaeae
RiyaOfTheSpectra <dhairya.bhandari@students.iiserpune.ac.in>
2025-05-15 16:36:10 +0530

    Added argument parsing.

--- LaserScanning.py
+++ LaserScanning.py
@@ -1,3 +1,4 @@
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import nidaqmx as ni
@@ -39,7 +40,7 @@ def Scan(
     z = np.zeros(np.shape(x))
 
     samps = len(x) // 3
-    rate = 1e3 / aq_time
+    rate = 3e3 / aq_time
     l = int(np.sqrt(samps))
 
     with (
@@ -72,3 +73,39 @@ def Scan(
         ao.start()
 
     return np.reshape(z, (l, l, 3))[:, :, 1]
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("amplitude", type=float, help="Amplitude of the beam, in degrees.")
+    parser.add_argument("time_step", type=float, help="Acquisition time for each pixel, in milliseconds.")
+    parser.add_argument("angl_step", type=float, help="Angular separation between pixels, in degrees.")
+    parser.add_argument('-o', "--output", help="Filename to save data.")
+    parser.add_argument('-s', "--show", help="Show map when done.", action="store_true")
+
+    args = parser.parse_args()
+
+    if not ( args.show & (args.output==None) ):
+        raise RuntimeError("The output will not be shown or saved, and will be forgotten, for ever…\nDon't.")
+
+    box = GenerateGrid(args.amplitude, step=args.angl_step)
+    z = Scan(box, args.time_step)
+    """
+    samps = len(z) // 3
+    l = int(np.sqrt(samps))
+    z = np.reshape(z, (l, l, 3))[:, :, 1]
+    """
+
+    if args.show:
+        plt.pcolormesh(z)
+        plt.gca().set_aspect('equal')
+        plt.show()
+        # TODO: Axes labelling.
+
+    if args.output:
+        np.savetxt(
+                args.output,
+                z,
+                delimiter=",",
+                header=f"# Amplitude = {args.amplitude}°,\n# Acquisition time = {args.time_step}s\n# Angular Resolution = {args.angl_step}°\n# "
+                )
+        # TODO: Check validity of file.
