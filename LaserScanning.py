03671d60af628b244ab36394e3f87a5f929d2042
Dhairya <dhairya.bhandari@students.iiserpune.ac.in>
2026-04-21 13:37:46 +0530

    Applied linter suggestions.

--- LaserScanning.py
+++ LaserScanning.py
@@ -1,23 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
-from datetime import datetime, timedelta
 import nidaqmx as ni
 from nidaqmx.constants import AcquisitionType, DigitalWidthUnits, TerminalConfiguration
-from time import sleep, monotonic
+from time import monotonic
 from math import floor
-import warnings
 
-from multiprocessing import Pipe
-from threading import Event
-from threading import Thread as Process
 
 def Scan(
     ampl,
-    step = 0.2,
-    aq_time_ms = 0.1, # time spent on each sample in milliseconds
-    dry = False,
-    average = 0,
+    step=0.2,
+    aq_time_ms=0.1,  # time spent on each sample in milliseconds
+    dry=False,
+    average=0,
 ):
     """
     Generates a grid of samples to write to the mirror setups.  Each sample is
@@ -33,10 +28,10 @@ def Scan(
     step = step / 0.8
 
     a = np.arange(
-            -ampl,
-            ampl + step,
-            step,
-            )
+        -ampl,
+        ampl + step,
+        step,
+    )
     l = len(a)
 
     frames = 2 + average
@@ -46,7 +41,7 @@ def Scan(
     x, y = (np.repeat(x, 2, axis=0), np.repeat(y, 2, axis=0))
     print(np.shape(x))
 
-    for i in range(l*2):
+    for i in range(l * 2):
         x[i] = x[i] if i % 2 == 0 else np.flip(x[i])
 
     if dry:
@@ -61,53 +56,46 @@ def Scan(
 
     if dry:
         z = x + y
-        z = np.reshape(z, (2*l, l, 2))[:, :, 1]
-        v = np.array([z[2*i] for i in range(l)])
+        z = np.reshape(z, (2 * l, l, 2))[:, :, 1]
+        v = np.array([z[2 * i] for i in range(l)])
         plt.imshow(v)
         plt.show()
         return
 
-    samps = len(x)# // 3
+    samps = len(x)  # // 3
     rate = 1e3 * frames / aq_time_ms
     scan_duration_ms = samps * aq_time_ms
 
     z = x + y
 
-    trigger_name = "/Dev1/ao/StartTrigger"
-
-    with (
-            ni.Task() as ao,
-            ni.Task() as ai
-            ):
-
+    with ni.Task() as ao, ni.Task() as ai:
         ao.ao_channels.add_ao_voltage_chan("Dev1/ao0:1")
 
-        ai.ai_channels.add_ai_voltage_chan("Dev1/ai0", terminal_config=TerminalConfiguration.DIFF)
+        ai.ai_channels.add_ai_voltage_chan(
+            "Dev1/ai0", terminal_config=TerminalConfiguration.DIFF
+        )
 
         ai.ai_channels["Dev1/ai0"].ai_rng_high = 1
         ai.ai_channels["Dev1/ai0"].ai_rng_low = -1
 
         ao.timing.cfg_samp_clk_timing(
-            rate,
-            sample_mode = AcquisitionType.FINITE,
-            samps_per_chan = samps
-            )
-        #ao.triggers.start_trigger.cfg_time_start_trig(now)
+            rate, sample_mode=AcquisitionType.FINITE, samps_per_chan=samps
+        )
 
         ai.timing.cfg_samp_clk_timing(
-            rate,
-            sample_mode = AcquisitionType.FINITE,
-            samps_per_chan = samps
-            )
-        ao.triggers.start_trigger.cfg_dig_edge_start_trig(ai.triggers.start_trigger.term)
+            rate, sample_mode=AcquisitionType.FINITE, samps_per_chan=samps
+        )
+        ao.triggers.start_trigger.cfg_dig_edge_start_trig(
+            ai.triggers.start_trigger.term
+        )
         ai.triggers.start_trigger.delay_units = DigitalWidthUnits.SAMPLE_CLOCK_PERIODS
         ai.triggers.start_trigger.delay = 0.5
 
         ao.write(np.array([x, y]), auto_start=False)
-        
+
         ao.start()
-        z = ai.read(samps, timeout = scan_duration_ms / 1e3)
-        ao.wait_until_done(timeout = scan_duration_ms / 1e3)
+        z = ai.read(samps, timeout=scan_duration_ms / 1e3)
+        ao.wait_until_done(timeout=scan_duration_ms / 1e3)
 
         ao.write([[0], [0]])
 
@@ -115,42 +103,13 @@ def Scan(
     # Hence, we get an array that is l x l x 3, and we choose the middle frame.
     print(np.shape(z))
     if average:
-        z = np.reshape(z, (2*l, l, frames))[:, :, 1:]
+        z = np.reshape(z, (2 * l, l, frames))[:, :, 1:]
         z = np.average(z, axis=2)
     else:
-        z = np.reshape(z, (2*l, l, frames))[:, :, 1]
-    v = np.array([z[2*i] for i in range(l)])
+        z = np.reshape(z, (2 * l, l, frames))[:, :, 1]
+    v = np.array([z[2 * i] for i in range(l)])
     return v
 
-def generate_sine_wave(
-    frequency: float,
-    amplitude: float,
-    sampling_rate: float,
-    number_of_samples: int,
-    phase_in: float = 0.0,
-):
-    """Generates a sine wave with a specified phase.
-
-    Args:
-        frequency: Specifies the frequency of the sine wave.
-        amplitude: Specifies the amplitude of the sine wave.
-        sampling_rate: Specifies the sampling rate of the sine wave.
-        number_of_samples: Specifies the number of samples to generate.
-        phase_in: Specifies the phase of the sine wave in radians.
-
-    Returns:
-        Indicates a tuple containing the generated data and the phase
-        of the sine wave after generation.
-    """
-
-    ampl = amplitude / 0.8
-
-    duration_time = number_of_samples / sampling_rate
-    duration_radians = duration_time * 2 * np.pi
-    phase_out = (phase_in + duration_radians) % (2 * np.pi)
-    t = np.linspace(phase_in, phase_in + duration_radians, number_of_samples, endpoint=False)
-
-    return (ampl * np.sin(frequency * t), phase_out)
 
 def CleanUp():
     with ni.Task() as ao:
@@ -159,73 +118,24 @@ def CleanUp():
         ao.wait_until_done()
     return
 
-def MirrorDance(sampling_rate, amplitude, frequency, kill_event, channel):
-    # TODO: channel selection input sanitisation.
-    with ni.Task() as task:
-        number_of_samples = 1000
-        task.ao_channels.add_ao_voltage_chan(f"Dev1/ao{channel}")
-        task.timing.cfg_samp_clk_timing(sampling_rate, sample_mode=AcquisitionType.CONTINUOUS)
 
-        actual_sampling_rate = task.timing.samp_clk_rate
-
-        data, _ = generate_sine_wave(
-            frequency,
-            amplitude,
-            sampling_rate=actual_sampling_rate,
-            number_of_samples=number_of_samples,
-        )
-        task.write(data)
-        task.start()
-        
-        kill_event.wait()
-
-        task.stop()
-
-def ReadAPD(trx_depth, kill_event, sndr, read_rate_hz):
-    with ni.Task() as apd:
-        data = np.zeros((2, trx_depth))
-        data[0, -1] = 5000
-        def callback(task_handle, every_n_samples_event_type, number_of_samples, callback_data):
-            """Callback function for reading signals."""
-            nonlocal data, sndr
-            term_x = data[0, -1]
-
-            xval = np.linspace(term_x + 1 / read_rate_hz, term_x + number_of_samples / read_rate_hz, number_of_samples) # Creates an extension to data[0].
-            read = apd.read(number_of_samples_per_channel=number_of_samples)
-
-            data = np.array([xval, read])
-            sndr.send(data)
-            return 0
-
-        apd.ai_channels.add_ai_voltage_chan("Dev1/ai0", terminal_config=TerminalConfiguration.DIFF)
-
-        apd.timing.cfg_samp_clk_timing(read_rate_hz, sample_mode=AcquisitionType.CONTINUOUS)
-        apd.register_every_n_samples_acquired_into_buffer_event(trx_depth, callback)
-        apd.start()
-
-        kill_event.wait()
-        sndr.close()
-
-        apd.stop()
-    return
-
-def AlignAPD(channel='0', frequency=1, amplitude=0.2, step=0.002):
+def AlignAPD(channel="0", frequency=1, amplitude=0.2, step=0.002):
     read_rate_hz = 20
     disp_duration_sec = 6
-    trx_depth = 100
-    p = 1 / frequency
+    period = 1 / frequency
 
-    disp_points = disp_duration_sec * read_rate_hz
     xlim = disp_duration_sec
 
     ampl = amplitude / 0.8
     step = step / 0.8
 
     with (
-            ni.Task() as apd,
-            ni.Task() as gal,
+        ni.Task() as apd,
+        ni.Task() as gal,
     ):
-        apd.ai_channels.add_ai_voltage_chan("Dev1/ai0", terminal_config=TerminalConfiguration.DIFF)
+        apd.ai_channels.add_ai_voltage_chan(
+            "Dev1/ai0", terminal_config=TerminalConfiguration.DIFF
+        )
         gal.ao_channels.add_ao_voltage_chan(f"Dev1/ao{channel}")
 
         fig, ax = plt.subplots()
@@ -236,7 +146,7 @@ def AlignAPD(channel='0', frequency=1, amplitude=0.2, step=0.002):
         add = True
         t_zero = monotonic()
 
-        line, = ax.plot([0], [0])
+        (line,) = ax.plot([0], [0])
 
         ax.set_title("Live APD Readout")
 
@@ -250,7 +160,7 @@ def AlignAPD(channel='0', frequency=1, amplitude=0.2, step=0.002):
             nonlocal line, gal, apd, val, add, t_zero
             t = monotonic() - t_zero
 
-            val = ampl * ( abs( 2 * ( t / p - floor( t / p + 1/2 ) ) ) - 1/2 )
+            val = ampl * (abs(2 * (t / period - floor(t / period + 1 / 2))) - 1 / 2)
 
             try:
                 gal.write(val)
@@ -263,8 +173,8 @@ def AlignAPD(channel='0', frequency=1, amplitude=0.2, step=0.002):
             x = np.append(x, [t])
             y = np.append(y, [volt])
 
-            x = x[-round(disp_duration_sec*read_rate_hz*1.5):]
-            y = y[-round(disp_duration_sec*read_rate_hz*1.5):]
+            x = x[-round(disp_duration_sec * read_rate_hz * 1.5) :]
+            y = y[-round(disp_duration_sec * read_rate_hz * 1.5) :]
 
             line.set_data(x, y)
 
@@ -278,32 +188,17 @@ def AlignAPD(channel='0', frequency=1, amplitude=0.2, step=0.002):
 
             left, right = ax.get_xlim()
             if t > right:
-                left  += 1
+                left += 1
                 right += 1
 
                 ax.set_xlim(left, right)
 
-            return (line, )
-
-        #kill_event = Event()
-        #kill_event.clear()
-
-        #read_proc = Process(target=ReadAPD, args=(trx_depth, kill_event, sndr, read_rate_hz))
-        #dance_proc = Process(target=MirrorDance, args=(read_rate_hz, amplitude, frequency, kill_event, channel))
-
-        #read_proc.start()
-        #dance_proc.start()
+            return (line,)
 
         try:
-            anne = anim.FuncAnimation(fig=fig, func=update, interval=10, blit=False)
+            anim.FuncAnimation(fig=fig, func=update, interval=10, blit=False)
             plt.show()
-            #kill_event.set()
-
-            #read_proc.join()
-            #dance_proc.join()
         except KeyboardInterrupt:
-            #kill_event.set()
-            print("Goodbye")
             exit
 
     return
